c207d91cddf7651f35f189ef6010e305da134341
lan.yu <lan.yu@changhong.com>
2021-03-04 14:14:36 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, escape, url_for, render_template
+from flask import Flask, escape, url_for, render_template, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 import click
 
 app = Flask(__name__)
+
+app.secret_key = '123456'
 
 db = SQLAlchemy(app)
 
@@ -22,6 +24,7 @@ def initdb(drop):
         db.drop_all()
     db.create_all()
     click.echo('Initialized database.')  # 输出提示信息
+
 
 # 注册为命令
 @app.cli.command()
@@ -60,16 +63,61 @@ class User(db.Model):
 
 
 class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    user = User.query.first()
-    movies = Movie.query.all()
-    return render_template('index_in_base.html', movies=movies)
+    if request.method == 'POST':  # 判断是否是 POST 请求
+        # 获取表单数据
+        title = request.form.get('title')  # 传入表单对应输入字段的 name 值
+        year = request.form.get('year')
+        # 验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')  # 显示错误提示
+            return redirect(url_for('index'))  # 重定向回主页
+        # 保存表单数据到数据库
+        movie = Movie(title=title, year=year)  # 创建记录
+        db.session.add(movie)  # 添加到数据库会话
+        db.session.commit()  # 提交数据库会话
+        flash('Item created.')  # 显示成功创建的提示
+        return redirect(url_for('index'))  # 重定向回主页
+
+    movie = Movie.query.all()
+    return render_template('index_in_base.html', movie=movie)
+
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    print('movie_id', movie_id)
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':  # 判断是否是 POST 请求
+        # 获取表单数据
+        title = request.form.get('title')  # 传入表单对应输入字段的 name 值
+        year = request.form.get('year')
+        # 验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')  # 显示错误提示
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回主页
+        # 保存表单数据到数据库
+        movie.title = title
+        movie.year = year
+        db.session.commit()  # 提交数据库会话
+        flash('Item updated.')  # 显示成功创建的提示
+        return redirect(url_for('edit', movie_id=movie.id))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)
+
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()  # 提交数据库会话
+    flash('Item delete.')  # 显示成功创建的提示
+    return redirect(url_for('index'))  # 重定向回主页
 
 
 @app.errorhandler(404)  # 传入要处理的错误代码
@@ -77,9 +125,9 @@ def page_not_found(e):  # 接受异常对象作为参数
     user = User.query.first()
     return render_template('404_in_base.html')  # 返回模板和状态码
 
+
 # 模板上下文处理函数
 @app.context_processor
 def inject_user():
     user = User.query.first()
     return dict(user=user)
-
