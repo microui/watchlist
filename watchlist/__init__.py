f139d261d02428dfc80c4b08623d12c91b2fcb99
lan.yu <lan.yu@changhong.com>
2021-03-06 14:15:25 +0800

    demo done

---
+++ watchlist/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from os import path
+
+
+app = Flask(__name__)
+login_manager = LoginManager(app)
+# 访问了设置登录保护的页面时，会自动跳转到登录页，需手动设置登录也的视图端点，不然会报错
+login_manager.login_view = 'login'
+# login_manager.login_message = '自定义错误提示'
+app.secret_key = '123456'
+db = SQLAlchemy(app)
+
+prefix = r'sqlite:///'
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + path.join(app.root_path, 'data.db')
+# 关闭对模型修改的监控
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+@login_manager.user_loader
+def load_user(user_id):  # 创建用户加载回调函数，接受用户 ID 作为参数
+    from watchlist.models import User
+    user = User.query.get(int(user_id))  # 用 ID 作为 User 模型的主键查询对应的用户
+    return user  # 返回用户对象
+
+
+# 模板上下文处理函数
+@app.context_processor
+def inject_user():
+    from watchlist.models import User
+    user = User.query.first()
+    return dict(user=user)
+
+
+from watchlist import commands, errors, models, views
--- watchlist/views.py
+++ watchlist/views.py
@@ -1,108 +1,8 @@
-from flask import Flask, escape, url_for, render_template, request, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
-from os import path
-from werkzeug.security import generate_password_hash, check_password_hash
-import click
+from flask import request, redirect, url_for, flash, render_template
+from flask_login import current_user, logout_user, login_user, login_required
 
-
-app = Flask(__name__)
-login_manager = LoginManager(app)
-
-# 访问了设置登录保护的页面时，会自动跳转到登录页，需手动设置登录也的视图端点，不然会报错
-login_manager.login_view = 'login'
-# login_manager.login_message = '自定义错误提示'
-app.secret_key = '123456'
-
-db = SQLAlchemy(app)
-
-prefix = r'sqlite:///'
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + path.join(app.root_path, 'data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
-
-
-# 注册为命令
-@app.cli.command()
-# 设置选项
-@click.option('--drop', is_flag=True, help='Create after drop.')
-def initdb(drop):
-    """Initialize the database."""
-    if drop:  # 判断是否输入了选项
-        db.drop_all()
-    db.create_all()
-    click.echo('Initialized database.')  # 输出提示信息
-
-
-# 注册为命令
-@app.cli.command()
-def forge():
-    """Generate fake data."""
-    db.create_all()
-
-    # 全局的两个变量移动到这个函数内
-    name = 'Grey Li'
-    movies = [
-        {'title': 'My Neighbor Totoro', 'year': '1988'},
-        {'title': 'Dead Poets Society', 'year': '1989'},
-        {'title': 'A Perfect World', 'year': '1993'},
-        {'title': 'Leon', 'year': '1994'},
-        {'title': 'Mahjong', 'year': '1996'},
-        {'title': 'Swallowtail Butterfly', 'year': '1996'},
-        {'title': 'King of Comedy', 'year': '1999'},
-        {'title': 'Devils on the Doorstep', 'year': '1999'},
-        {'title': 'WALL-E', 'year': '2008'},
-        {'title': 'The Pork of Music', 'year': '2012'},
-    ]
-
-    user = User(name=name)
-    db.session.add(user)
-    for m in movies:
-        movie = Movie(title=m['title'], year=m['year'])
-        db.session.add(movie)
-
-    db.session.commit()
-    click.echo('Done.')
-
-
-@login_manager.user_loader
-def load_user(user_id):  # 创建用户加载回调函数，接受用户 ID 作为参数
-    user = User.query.get(int(user_id))  # 用 ID 作为 User 模型的主键查询对应的用户
-    return user  # 返回用户对象
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-    username = db.Column(db.String(20))
-    password_hash = db.Column(db.String(128))
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def validate_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-@app.cli.command()
-@click.option('--username', prompt=True, help='The username used to login.')
-@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
-def admin(username, password):
-    db.create_all()
-    user = User.query.first()
-    if user is not None:
-        user.username = username
-        user.set_password(password)
-    else:
-        user = User(username=username, name='admin')
-        user.set_password(password)
-        db.session.add(user)
-    db.session.commit()
-    click.echo('Done')
-
-
-class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(60))
-    year = db.Column(db.String(4))
+from watchlist import app, db
+from watchlist.models import Movie, User
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -124,6 +24,7 @@ def index():
         db.session.commit()  # 提交数据库会话
         flash('Item created.')  # 显示成功创建的提示
         return redirect(url_for('index'))  # 重定向回主页
+
     movie = Movie.query.all()
     return render_template('index_in_base.html', movie=movie)
 
@@ -212,10 +113,3 @@ def delete(movie_id):
 def page_not_found(e):  # 接受异常对象作为参数
     user = User.query.first()
     return render_template('errors/404.html')  # 返回模板和状态码
-
-
-# 模板上下文处理函数
-@app.context_processor
-def inject_user():
-    user = User.query.first()
-    return dict(user=user)
